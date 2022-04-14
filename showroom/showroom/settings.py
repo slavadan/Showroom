29e173cb8bb33c8a0645b5b831dbef26c31a9769
slavadan <slavadan909@gmail.com>
2022-04-14 13:12:17 +0300

    install django-rest

--- showroom/showroom/settings.py
+++ showroom/showroom/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    'rest_framework',
     "car_showroom",
     "users",
     "sales",
@@ -136,3 +136,11 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
